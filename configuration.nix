1b7c4de4137366b77f4f6939a5c0364dc2bcec55
Cronos6096 <99344213+Cronos6096@users.noreply.github.com>
2025-02-16 14:24:46 +0100

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -57,13 +57,12 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  #  services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
   
-  #  services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma6.enable = true;
-
+  # services.xserver.enable = true;
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma6.enable = true;  
 
   # Configure keymap in X11
   services.xserver.xkb = {
